759e291c8eca8972860247e39c98e259f8198aae
Richard Evans <rickecon@gmail.com>
2017-06-06 09:07:48 +0200

    Cleaned up some Day6, Ch. 14 stuff.

--- Day6_TaxFunc/in_class_stuff/taxfunc.py
+++
@@ -1,9 +0,0 @@
-# Import files
-import pandas as pd
-import pickle
-
-# Read in the data
-# TaxFunc_pkl = 'taxdata42.pkl'
-# with open(TaxFunc_pkl, 'rb') as f:
-#     dict_params = pickle.load(f, encoding='latin1')
-micro_data = pickle.load(open('taxdata42.pkl', 'rb'))
